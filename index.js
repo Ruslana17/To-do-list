5277d6e55e2ab076b51c2c27123a9e16d4a8fb59
Rusłana Fankowska <ruslana.fankovskaya@gmail.com>
2023-07-21 12:44:51 +0200

    update

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ let letter = document.getElementById('input_text');
 letter.onkeydown = function(event) {
 	this.value = this.value.charAt(0).toUpperCase() + this.value.slice(1);
 }
+
+/* fuction creat to-do*/
+const area=document.querySelector('#span_area_list');
+console.log(area);
