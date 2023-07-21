e6ceb8eca2f7abdd0ea08c5efd3ca7c08cbeaf1f
Rusłana Fankowska <ruslana.fankovskaya@gmail.com>
2023-07-21 14:34:09 +0200

    update

--- index.js
+++ index.js
@@ -5,5 +5,45 @@ letter.onkeydown = function(event) {
 }
 
 /* fuction creat to-do*/
-const area=document.querySelector('#span_area_list');
-console.log(area);
+const areaList = document.querySelector('#span_area_list');
+const list = document.querySelector('#list'); /*ul */
+const button= document.querySelector('#input_submit');/*button submit */
+const areaForText = document.querySelector('#input_text');/* text area */
+
+
+function newElement(){
+	let li= document.createElement("li");
+	let inputValue= areaForText.value;
+	let t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if(inputValue === ""){
+		alert("You must write something!");
+	}else { 
+		list.appendChild(li);
+
+	}
+	areaForText.value="";
+
+
+	/*let span =document.createElement("SPAN");
+	let txt =document.createTextNode("/uooD7");*/
+
+
+}
+
+
+
+
+
+
+
+
+/*function addToDoText(){
+ 	list.style.marginTop="3vw";
+	areaForText.push(button.value);
+ 	list.innerHTML="";
+ 	areaForText.forEach(i => {
+	list.innerHTML+=`<li>${i}</li>`;
+ });
+}
+addToDoText();*/
